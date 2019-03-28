c4cadb954049d477540ab66b0d13e1195abd1cf7
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-03-28 23:44:14 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -14,8 +14,8 @@ def greeting(greeting, name)
   puts "#{greeting}, #{name}"
 end
 
-def return_a_value(phrase="Nice")
-  phrase
+def return_a_value
+  Nice
 end
 
 def last_evaluated_value(value="expert")
