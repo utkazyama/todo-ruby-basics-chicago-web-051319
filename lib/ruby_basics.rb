1452bb21c020eed33d356bd39df55976e72ac0d8
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-03-28 23:40:27 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(value="argue")
+  value
 end
 
-def greeting
-
+def greeting(greeting, name)
+  puts greeting, name
 end
 
-def return_a_value
-
+def return_a_value(phrase="Nice")
+  phrase
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(value="expert")
+  value
 end
 
-def pizza_party
-
+def pizza_party(pizza="cheese")
+  pizza
 end
